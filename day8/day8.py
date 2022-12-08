6e3509e1bac5ac6f7b3619832cfc3ba1efb694f9
Pierre-Luc Robitaille <2012805@cegeplevis.ca>
2022-12-08 12:56:16 -0500

    optimisation

--- day8/day8.py
+++ day8/day8.py
@@ -1,27 +1,46 @@
 inputpath = "/Users/pierr/Desktop/Funny/adventofcode2022/day8/input.txt"
 map = open(inputpath, "r").readlines()
 # tree = [[int(c) for c in line] for line in map]
-total = 0
 
 
 def part_one():
+    total = 0
+    previousTree = []
+    nextTree = []
     for index, line in enumerate(map):
-        if (map[index] == map[-1] or index == 0):
+        if index == 0:
+            total += len(map[index]) - 1
+            previousTree.extend([[int(x) for x in tree]
+                                for tree in map[index][:-1]])
+            nextTree.extend([[int(x) for x in tree]
+                            for tree in map[index+1][:-1]])
+            for inde, _ in enumerate(nextTree):
+                nextTree[inde].extend([eval(tree[inde])
+                                      for tree in map[index+2:]])
+            continue
+        if (map[index] == map[-1]):
             total += len(map[index]) - 1
             continue
         for num, char in enumerate(line):
             if char == "\n":
                 continue
+            if num == 0:
+                nextTree[num+1].pop(0)
+
             if (num == 0 or line[num+1] == "\n"):
+                previousTree[num].append(eval(char))
                 total += 1
                 continue
-
-            previousTrees = [eval(tree[num]) for tree in map[:index]]
-            nextTrees = [eval(tree[num]) for tree in map[index+1:]]
+            previousTrees = previousTree[num]
+            #previousTrees = [eval(tree[num]) for tree in map[:index]]
+            nextTrees = nextTree[num]
             currentPreviousTrees = [eval(num) for num in list(line[:num])]
             currentNextTrees = [eval(num) for num in list(line[num+1:-1])]
             if eval(char) > max(previousTrees) or eval(char) > max(nextTrees) or eval(char) > max(currentNextTrees) or eval(char) > max(currentPreviousTrees):
                 total += 1
+            previousTree[num].append(eval(char))
+            nextTree[num+1].pop(0)
+
     return (total)
 
 
@@ -69,4 +88,5 @@ def part_two():
     return max(scenicScore)
 
 
+print(part_one())
 print(part_two())
