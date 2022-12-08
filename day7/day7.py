a6c0b82694aa684c70f8753f8f877f3363db85ea
Pierre-Luc Robitaille <2012805@cegeplevis.ca>
2022-12-08 00:23:01 -0500

    god damn that second part was easy

--- day7/day7.py
+++ day7/day7.py
@@ -3,11 +3,11 @@ import re
 
 inputpath = "/Users/pierr/Desktop/Funny/adventofcode2022/day7/input.txt"
 commands = open(inputpath, "r").read().splitlines()
+directory = []
+filesizes = {}
 
 
 def part_one():
-    directory = []
-    filesizes = {}
     for command in commands:
         if command.startswith("$ cd"):
             if ".." in command:
@@ -28,3 +28,16 @@ def part_one():
         if filesizes.get(keys) <= 100000:
             list_of_.append(filesizes.get(keys))
     return (sum(list_of_))
+
+
+def part_two():
+    minimumSpace = 30000000 - (70000000 - filesizes.get('/'))
+    listPossible = []
+    for values in filesizes.values():
+        if values >= minimumSpace:
+            listPossible.append(values)
+    return (min(listPossible))
+
+
+part_one()
+print(part_two())
