f2b40d9eacba1ffeea89501e308a91b744fc2583
Pierre-Luc Robitaille <2012805@cegeplevis.ca>
2022-12-12 19:35:18 -0500

    i quit

--- day9/day9.py
+++ day9/day9.py
@@ -39,7 +39,7 @@ class Tail():
         self.distance = 0
         self.movements = []
 
-    def move(self, direction, movement, length, head: Head):
+    def move(self, direction, movement, length, head):
         h_distance = (head.x-self.x)
         v_distance = (head.y-self.y)
         prevy = self.y
@@ -125,10 +125,70 @@ class Tail():
 
 
 class Knot():
-    def __init__(self, head: Head) -> None:
+    def __init__(self) -> None:
         self.x = 0
         self.y = 0
-        self.Head = head
+        self.distance = 0
+        self.movements = []
+
+    def move(self, direction, movement, length, head):
+        h_distance = (head.x-self.x)
+        v_distance = (head.y-self.y)
+        prevy = self.y
+        prevx = self.x
+        l_coord = []
+        match direction:
+            case "U":
+                if (self.y+length >= head.y):
+                    return
+                elif (self.y >= head.y - movement):
+                    self.y += v_distance-length
+                else:
+                    self.y += movement
+
+                if (self.x < head.x):
+                    self.x += 1
+                elif (self.x > head.x):
+                    self.x -= 1
+
+            case "D":
+                if (self.y-length <= head.y):
+                    return
+                elif (self.y <= head.y + movement):
+                    self.y -= (-v_distance)-length
+                else:
+                    self.y -= movement
+
+                if (self.x < head.x):
+                    self.x += 1
+                elif (self.x > head.x):
+                    self.x -= 1
+
+            case "R":
+                if (self.x+length >= head.x):
+                    return
+                elif (self.x >= head.x - movement):
+                    self.x += h_distance-length
+                else:
+                    self.x += movement
+
+                if (self.y < head.y):
+                    self.y += 1
+                elif (self.y > head.y):
+                    self.y -= 1
+
+            case "L":
+                if (self.x-length <= head.x):
+                    return
+                elif (self.x <= head.x + movement):
+                    self.x -= (-h_distance)-length
+                else:
+                    self.x -= movement
+
+                if (self.y < head.y):
+                    self.y += 1
+                elif (self.y > head.y):
+                    self.y -= 1
 
 
 head = Head()
@@ -144,19 +204,37 @@ def part_one(length):
     return (len(visited))
 
 
-def part_two(length):
+def part_two():
+    l_instruction = []
     for instruction in data:
-        head.move(instruction[0], int(instruction[1]))
-        tail.move(instruction[0], int(instruction[1]), length, head)
+        l_instruction.append(instruction)
+        if (len(l_instruction) > 10):
+            l_instruction.pop(0)
+
+        head.move(l_instruction[0][0], int(l_instruction[0][1]))
+        for index, knots in enumerate(l_knot):
+            if (index == 0):
+                knots.move(l_instruction[index][0],
+                           int(l_instruction[index][1]), 1, head)
+            else:
+                if (index > len(l_instruction)-1):
+                    continue
+                knots.move(l_instruction[index][0], int(
+                    l_instruction[index][1]), 1, l_knot[index-1])
+        if (9 > len(l_instruction)-1):
+            continue
+        tail.move(l_instruction[9][0], int(l_instruction[9][1]), 1, l_knot[-1])
     return (len(visited))
 
 
 print(part_one(1))
 head = Head()
 tail = Tail()
+l_knot = [Knot() for _ in range(0, 9)]
+
 visited.clear()
 visit = {str((tail.x, tail.y)): "visited"}
 visited.update(visit)
 
 
-print(part_two(10))
+print(part_two())
