1c65ed557b2582f0a08f7d4dd2256cd276f79db1
Pierre-Luc Robitaille <2012805@cegeplevis.ca>
2022-12-15 19:19:21 -0500

    part two

--- day10/day10.py
+++ day10/day10.py
@@ -6,7 +6,7 @@ index = 0
 register = 1
 currentcyle = []
 numbers = 0
-while cycle < 221:
+while cycle < 241:
     if actions[index][0] == 'noop':
         if len(currentcyle) == 1:
             index += 1
@@ -24,5 +24,11 @@ while cycle < 221:
             currentcyle.append(cycle)
     if cycle % 40 == 20:
         numbers += cycle*register
+    if (((cycle-1) % 40) == register+1 or ((cycle-1) % 40) == register or ((cycle-1) % 40) == register-1):
+        print('#', end='')
+    else:
+        print(' ', end='')
+    if (cycle % 40 == 0):
+        print('')
     cycle += 1
 print(numbers)
